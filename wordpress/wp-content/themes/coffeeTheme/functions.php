8cd8593c4ebe1518511f853d6d1bdbe1009a4b78
webbkd <kdw1995@live.com>
2018-01-12 20:04:27 -0500

    Adding the functions and the event page to the wordpress theme

---
+++ wordpress/wp-content/themes/coffeeTheme/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+ //Navigation Menus
+ register_nav_menus(array(
+    'primary' => __('Primary Menu')	
+ ));
+
+//adding feature image support
+add_theme_support('post-thumbnails');
+
+
+?>
