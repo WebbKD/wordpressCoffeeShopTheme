08eaa3cea3b738905e211f8857debc9aa190f2a9
webbkd <kdw1995@live.com>
2018-01-20 13:04:21 -0500

    updating the index

--- wordpress/wp-content/themes/coffeeTheme/index.php
+++ wordpress/wp-content/themes/coffeeTheme/index.php
@@ -7,7 +7,7 @@ get_header();
     if (have_posts()) :
         while (have_posts()) : the_post(); 
 
-            //the_content();
+            
          get_template_part('content', get_post_format());
         
          endwhile; 
